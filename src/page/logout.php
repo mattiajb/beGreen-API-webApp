4d1bb3504a744354eae56a1d788a820b7ad7583c
mattiajb <bavaromg@gmail.com>
2026-02-03 21:01:17 +0100

    Add comments to logout.php

--- src/page/logout.php
+++ src/page/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 session_unset();
-session_destroy();
-header("Location: home.php");
+session_destroy(); // Termina la sessione ed elimina le variabili associate
+header("Location: home.php"); // Reindirizza l'utente alla home dopo l'operazione di logout
 exit();
 ?>
